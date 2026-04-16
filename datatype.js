92e8c4145465fc9373a8d5408c508f76b0a971b9
Palkesh Tanwar <palakeshtanwar@gmail.com>
2026-04-16 05:26:50 +0000

    DATA TYPE

--- datatype.js
+++ datatype.js
@@ -1,1 +1,17 @@
-alert(3+3)
+// alert(3+3) we are using node.js not browser.
+
+let name = "Palkesh" //string
+
+let age = 18 // numbeer
+
+let loggedIn = true // boolean
+
+console.table([name, age, loggedIn])
+
+//null => stand alone value => OBJECT TYPE
+
+//undefined => value dosn't defined/assigned
+
+//symbol => uniqe 
+
+//OBJECT =>
