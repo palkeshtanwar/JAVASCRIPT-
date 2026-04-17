a656dc0c713b39f9ea0a65fbf0e43b6536ff5c5e
Palkesh Tanwar <palakeshtanwar@gmail.com>
2026-04-17 05:43:09 +0000

    COMPARISION

---
+++ Comparision.js
@@ -0,0 +1,21 @@
+// console.log(1<2)
+// console.log(1<=2)
+// console.log(1>2)
+// console.log(1==2)
+// console.log(1 !=2)
+
+console.log("2">1)
+
+// AVOID THE BELOW GIVEN COMPARISON 
+
+console.log(null > 0)
+console.log(null >= 0)
+console.log(null == 0)
+
+console.log(undefined > 0)
+console.log(undefined >= 0)
+console.log(undefined == 0)
+
+// === STRICT CHECKING => ALSO CHECK DATA TYPE
+
+console.log("2" === 2)
