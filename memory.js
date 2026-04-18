c9a01dfa91b096a4b75f9f55c4e5f166fb7986bb
Palkesh Tanwar <palakeshtanwar@gmail.com>
2026-04-18 09:34:11 +0000

    STACK AND HEAP MEMORY

---
+++ memory.js
@@ -0,0 +1,18 @@
+// STACK MEMORY & HEAP MEMORY
+
+//1. STACK MEMORY => PRIMITIVE => gives copy
+
+let myName = "Palkesh"
+
+let anotherName = myName
+
+anotherName = "Tanwar"
+
+console.log(myName)
+console.log(anotherName)
+
+
+
+//2. HEAP MEMORY => NON PRIMITIVE => gives reference
+
+
