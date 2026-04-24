28365fe165b92a5cfd5c68976302abd6a9f55d00
Palkesh Tanwar <palakeshtanwar@gmail.com>
2026-04-24 05:32:57 +0000

    Number and Maths

---
+++ numbers.js
@@ -0,0 +1,33 @@
+
+// NUMBERS AND MATHS
+
+const score = 400
+
+const balance = new Number(100)
+
+// console.log(balance);
+// console.log(balance.toFixed(2));
+// console.log(balance.toPrecision(3));
+
+const hundered = 100000 ;
+
+// console.log(hundered.toLocaleString('EN-in'))
+
+
+//////////////////////////////////////MATHS///////////////////////////////////////////////////////////
+
+console.log(Math);
+console.log(Math.abs(-5));
+console.log(Math.round(5.567));
+console.log(Math.ceil(5.567));
+console.log(Math.floor(5.567));
+console.log(Math.min(5,7,87,34,2,));
+console.log(Math.max(5,34,56,89,453,));
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+const min = 10
+
+const max = 200
+
+console.log(Math.floor(Math.random() * (max-min+1)) +min)
