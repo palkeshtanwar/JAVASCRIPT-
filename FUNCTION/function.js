8845d68d4f9a91decb125dfa6b1a906ab9c062fa
Palkesh Tanwar <palakeshtanwar@gmail.com>
2026-05-04 05:10:56 +0000

    BASICS OFFUNCTION

---
+++ FUNCTION/function.js
@@ -0,0 +1,19 @@
+function myName() {
+    console.log("P")
+    console.log("A")
+    console.log("L")
+    console.log("K")
+    console.log("E")
+    console.log("S")
+    console.log("H")
+}
+
+// myName()
+
+function addNum(a,b) {
+     console.log(a+b)
+    return a+b;
+   
+}
+addNum(4,5)
+
