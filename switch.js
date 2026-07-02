92ed254460f4d2ae152f20f00e0ac4a7f8a6038c
Palkesh Tanwar <palakeshtanwar@gmail.com>
2026-07-02 03:44:12 +0000

    Terniary Operator

--- switch.js
+++ switch.js
@@ -1,44 +1,44 @@
-const month = 7
+// const month = 7
 
-switch (month) {
-    case 1:
-        console.log("January");
+// switch (month) {
+//     case 1:
+//         console.log("January");
         
-        break;
-    case 2:
-        console.log("Febuary");
+//         break;
+//     case 2:
+//         console.log("Febuary");
         
-        break;
-    case 3:
-        console.log("March");
+//         break;
+//     case 3:
+//         console.log("March");
         
-        break;
-    case 4:
-        console.log("Apirl");
+//         break;
+//     case 4:
+//         console.log("Apirl");
         
-        break;
-    case 5:
-        console.log("May");
+//         break;
+//     case 5:
+//         console.log("May");
         
-        break;
-    case 6:
-        console.log("June");
+//         break;
+//     case 6:
+//         console.log("June");
         
-        break;
-    case 7:
-        console.log("July");
+//         break;
+//     case 7:
+//         console.log("July");
         
-        break;
-    case 8:
-        console.log("August");
+//         break;
+//     case 8:
+//         console.log("August");
         
-        break;
+//         break;
 
-    default:
-        console.log("default value");
+//     default:
+//         console.log("default value");
         
-        break;
-}
+//         break;
+// }
 
 // falsy value 
 
@@ -47,6 +47,28 @@ switch (month) {
 // truthy value
 
 // "0", "false", " ", [], {}, function(){}, 
+// NULLISH COALESCING OPERATOR (??) => NULL UNDEFINED
 
+let val1 ;
+let val2 ;
+let val3 ;
+let val4 ;
 
+val1 = null ?? 10
+val2 = undefined ?? 10
+val3 = 7 ?? 10
+val4 = null ?? 10 ?? 20
+
+console.log(val1);
+console.log(val2);
+console.log(val3);
+console.log(val4);
+
+//  Terniary Operator 
+
+// condition ? true : false
+
+const price = 100
+
+price >= 80 ? console.log("less then 80") : console.log("more then 80")
 
