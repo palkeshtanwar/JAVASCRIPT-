7fe1c9dd7b00095814954c474a95d9f934c5536a
Palkesh Tanwar <palakeshtanwar@gmail.com>
2026-07-02 03:35:46 +0000

    Switch in jS

---
+++ switch.js
@@ -0,0 +1,52 @@
+const month = 7
+
+switch (month) {
+    case 1:
+        console.log("January");
+        
+        break;
+    case 2:
+        console.log("Febuary");
+        
+        break;
+    case 3:
+        console.log("March");
+        
+        break;
+    case 4:
+        console.log("Apirl");
+        
+        break;
+    case 5:
+        console.log("May");
+        
+        break;
+    case 6:
+        console.log("June");
+        
+        break;
+    case 7:
+        console.log("July");
+        
+        break;
+    case 8:
+        console.log("August");
+        
+        break;
+
+    default:
+        console.log("default value");
+        
+        break;
+}
+
+// falsy value 
+
+// false ,0, -0, bigint 0n, "", null, undefined, not a number
+
+// truthy value
+
+// "0", "false", " ", [], {}, function(){}, 
+
+
+
