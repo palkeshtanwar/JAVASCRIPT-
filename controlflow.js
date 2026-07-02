f0c23c91191073d3a561df29d62baf84fd0faa15
Palkesh Tanwar <palakeshtanwar@gmail.com>
2026-07-02 03:06:00 +0000

    Control Flow

---
+++ controlflow.js
@@ -0,0 +1,28 @@
+// if 
+
+// const value = 4
+
+// if (value == 2) {
+//    console.log("Equal")
+// } else {
+//     console.log("not equal")
+// } 
+
+
+// const score = 72
+
+// if (score < 50) {
+//     console.log("less then 50")
+// } else if (score < 60) {
+//     console.log("less then 60")
+// }
+//   else if (score < 70) {
+//     console.log("less then 70")
+// }
+//   else if (score < 80) {
+//     console.log("less then 80")
+// }
+//   else {
+//     console.log("less then 90")
+// }
+
