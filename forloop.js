5c4d0861db1178a7b65e22a2196f48a6733255a8
Palkesh Tanwar <palakeshtanwar@gmail.com>
2026-07-03 03:26:15 +0000

    Iterations

---
+++ forloop.js
@@ -0,0 +1,31 @@
+// for (let i = 0; i<= 10; i++) {
+//     const element = i;
+//     console.log(element)
+    
+// }
+
+
+// BREAK AND CONTINUE
+
+// for (let i = 0; i <= 20; i++) {
+//     if (i == 5) {
+//         console.log("5 is find")
+//         break
+//     }
+//     console.log(`value of i is: ${i}`)
+// }
+
+// for (let i = 0; i <= 20; i++) {
+//     if (i == 5) {
+//         console.log("5 is find")
+//         continue
+//     }
+//     console.log(`value of i is: ${i}`)
+// }
+
+for (let i = 1; i <= 10; i++) {
+    // if (i) {
+        console.log(i*2)
+    // }
+    // console.log(i*2)
+}
